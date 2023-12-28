d26f6e9d28e54ab7bcd786c3a4778328ccd37f6d
carlos_wendel <wendeluzumak824@gmail.com>
2023-12-28 14:42:53 -0300

    Iniciação do projeto

---
+++ secretword/src/data/word.js
@@ -0,0 +1,8 @@
+export const wordList ={
+    carro: ["motor","Porta","Capô","Pneu","Antena"],
+    fruta: ["Banana","Maça","Pêra","Mamão","Laranja"],
+    corpo: ["Braço","Perna","Cérebro","Pescoço","Olhos"],
+    computador: ["Mouse","Teclado","Monitor","Gabinete","Processador"],
+    programação: ["Linguagem","Framerwork","Javascript","React","lógica"],
+    alimento: ["Arroz","Feijão","Carne","Leite","Ovo"]
+}
